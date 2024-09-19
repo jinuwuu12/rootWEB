3cf5bda287f153f0658c06a18a3baeddc420fcf0
jinuwuu <jin82232@naver.com>
2024-09-19 18:05:06 +0900

    extra_error_is_solved

--- scannerApp/models.py
+++ scannerApp/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
-<<<<<<< HEAD
 # models.Model 은 Django 내부의 데이터베이스 테이블과 상호작용을 위한 기본 클래스 상속
 class barcode_info(models.Model):
     barcode_num = models.IntegerField()
-=======
-# Create your models here.
->>>>>>> 1a609de7279d451843ed072be8af0027c4d0c2a8
+
--- scannerApp/urls.py
+++ scannerApp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.upload_page_view),
     path('upload-barcode/', views.barcode_reading_view, name='upload_barcode'),
-    # path('upload_page/' , views.upload_page_view, name='upload_page')
 ]
+
