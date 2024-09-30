1056894775f0211e2710c4b7d2977f74c23455c7
jinuwuu <jin82232@naver.com>
2024-10-01 00:58:01 +0900

    0

--- config/config.py
+++
@@ -1,7 +0,0 @@
-database = 'scanner_db'
-user = 'root'
-password = 'jinwoo123@'
-host = '127.0.0.1'
-port = '3307'
-
-SECRET_KEY = "django-insecure-tisnjohjh+@skdu%b9jfjiv($a@(w_3h)p*%a733eq4%^3ams*"
