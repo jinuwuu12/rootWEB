9101404a3161bde9222eb866ac383030d3b86bdd
PYB <ad07829@naver.com>
2024-10-14 23:23:15 +0900

    inventory check And download_excel

--- inventorycheckApp/urls.py
+++ inventorycheckApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("IC_Main/", views.index, name='IC_Main'),
+    path("IC_Main/userID/",views.index_with_userid, name = 'IC_Main_userID'),
+    path("IC_Main/download_excel/", views.download_excel, name='download_excel'),
+    
 ]
